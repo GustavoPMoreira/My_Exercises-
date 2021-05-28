9b5083ddbaa62751cb0ace61a78eb2736e9c8850
Gustavo Moreira <gustavopaiva15@gmail.com>
2021-05-28 19:43:14 -0300

    Pilha em C feita!

--- C/pilha.c
+++ C/pilha.c
@@ -1,31 +1,70 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define tam 10
+#define MAX_SIZE 10
 
-typedef struct pilha{
-    int dado[tam];
-    int primeiro;
-    int ultimo;
-    }pilha;
+typedef struct stack
+{
+    int values[MAX_SIZE];
+    int top;
+} stack;
 
-pilha p;
-void PUSH(int elemento){
+stack my_stack;
 
+void start_stack(stack *pointer)
+{
+    //a seta significa: acessando a propriedade último da struct p
+    pointer->top = -1;
 }
-void POP(){
-    p.dado[p.ultimo]=0;
-    p.ultimo--;
-}
-void DISPLAY(){
 
+void push(stack *pointer, int value)
+{
+    //criou-se um parâmetro que é um ponteiro para uma stack e o outro é o value para inserção
+    //se o valor da ultima posição for igual ao tamnho-1, a stack tá cheia
+    if (pointer->top == MAX_SIZE - 1)
+    {
+        printf("stack FULL!\n");
+    }
+    else
+    {
+        pointer->top++;
+        pointer->values[pointer->top] = value;
+    }
 }
-int main(){
 
+int pop(stack *pointer)
+{
+    if (pointer->top == -1)
+    {
+        printf("stack EMPTY!\n");
+    }
+    else
+    {
+        int value=pointer->values[pointer->top];
+        pointer->top--;
+        return value;
+    }
+}
+
+void display(stack *pointer)
+{
+    int i= pointer->top;
+    while (i>=0)
+    {
+        printf("Value in position %d: %d \n", i, pointer->values[i]);
+        i--;
+    }
+}
+
+int main()
+{
+    start_stack(&my_stack);
+    push(&my_stack,5);
+    push(&my_stack,10);
+    push(&my_stack,15);
+    int value=pop(&my_stack);
+    printf("Value popped: %d \n", value);
+    display(&my_stack);
+    system("pause");
     return 0;
 }
-
-/*FIXME
-adicionar a função PUSH
-adicionar a função DISPLAY
-adicionar o main*/
